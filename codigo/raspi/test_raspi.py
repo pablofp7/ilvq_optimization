c6c3d9807ce835a34284da1e49aa979d604fd968
pablofp7 <pablofernandezpi@gmail.com>
2024-02-23 13:38:17 +0100

    ZeroMQ. Raspi Node

--- codigo/raspi/test_raspi.py
+++ codigo/raspi/test_raspi.py
@@ -104,6 +104,9 @@ if __name__ == "__main__":
         
         iteraciones = 50
         datasets = ["elec", "phis", "elec2"]
+        
+        iteraciones = 20
+        datasets = ["phis", "elec2"]
 
         data_name = {"elec": "electricity.csv", "phis": "phishing.csv", "elec2": "electricity.csv"}
         
