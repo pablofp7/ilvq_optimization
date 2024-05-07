98179a364a530d3ade7e7f7e8a84cc68d2ff6268
pablofp7 <pablofernandezpi@gmail.com>
2024-05-07 18:20:45 +0200

    Uniendo test3

--- codigo/raspi/pc_launcher/send_commands.py
+++ codigo/raspi/pc_launcher/send_commands.py
@@ -1,5 +1,6 @@
 import paramiko
 import sys
+import argparse
 
 def send_command(hosts, command, username='pablo', password='123', sudo_password='123'):
     for host in hosts:
@@ -34,7 +35,26 @@ def send_command(hosts, command, username='pablo', password='123', sudo_password
             session.close()
             client.close()
 
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser(description="Send a command to multiple hosts via SSH")
+    parser.add_argument("command", help="The command to execute on the hosts")
+    parser.add_argument("-n", "--num-nodes", type=int, help="Number of nodes to target")
+    parser.add_argument("-s", "--specific-nodes", help="Comma-separated list of specific node indices")
+
+    args = parser.parse_args()
+
     hosts = [f"nodo{i}.local" for i in range(5)]
-    command = sys.argv[1] if len(sys.argv) > 1 else "echo 'Hello from Raspberry Pi!'"
-    send_command(hosts, command)
+
+    if args.num_nodes is not None:
+        hosts = hosts[:args.num_nodes]
+    elif args.specific_nodes is not None:
+        specific_indices = [int(idx) for idx in args.specific_nodes.split(',')]
+        hosts = [hosts[i] for i in specific_indices]
+
+    send_command(hosts, args.command)
+
+if __name__ == "__main__":
+    main()
+
+if __name__ == "__main__":
+    main()
